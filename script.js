b7ad3d7208a4f7db220f1bf386c40b7c3034dbcd
Guruprasad s s <gprasadss03@gmail.com>
2024-06-26 08:23:36 +0530

    Added All Sections in html

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// git push origin main 
+document.addEventListener('DOMContentLoaded',()=>{
+  let display = document.getElementById('section1');
+  display.classList.add('active');
+})
+
+function Activate(sectionId){
+  let sectionToShow = document.querySelectorAll();
+  sectionToShow.
+}
